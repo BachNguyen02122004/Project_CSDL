a64dd99cc3f446cd97a3d55a1c089c5b84608a21
TrMiDUc <22026525@vnu.edu.vn>
2023-10-07 02:30:49 +0900

    update

--- php/update_product.php
+++ php/update_product.php
@@ -13,16 +13,32 @@ if ($conn->connect_error) {
     die("Kết nối đến cơ sở dữ liệu thất bại: " . $conn->connect_error);
 }
 
-
+//get data
 $data = json_decode(file_get_contents("php://input"));
 
+$check = "SELECT * FROM cart WHERE productId = '$data->productId'";
+$result = mysqli_query($conn, $check);
 
-$sql = "INSERT INTO cart (id, productId, productName, productImage, quantity) VALUES ('$data->id', '$data->productId', '$data->name', '$data->image', '$data->quantity')";
+//adding to cart
+if ($result->num_rows == 0) {
+    $sql = "INSERT INTO cart (id, productId, productName, productImage, quantity) VALUES ('$data->id', '$data->productId', '$data->name', '$data->image', '$data->quantity')";
 
-if ($conn->query($sql) === TRUE) {
-    $response = "Dữ liệu đã được lưu vào cơ sở dữ liệu thành công";
+    if ($conn->query($sql) === TRUE) {
+        $response = "Dữ liệu đã được lưu vào cơ sở dữ liệu thành công";
+    } else {
+        $response = "Lỗi khi lưu dữ liệu vào cơ sở dữ liệu: " . $conn->error;
+    }
+
 } else {
-    $response = "Lỗi khi lưu dữ liệu vào cơ sở dữ liệu: " . $conn->error;
+
+    $sql = "UPDATE cart SET quantity = (quantity + $data->quantity) WHERE productId = '$data->productId'";
+
+
+    if ($conn->query($sql) === TRUE) {
+        $response = "Dữ liệu đã được lưu vào cơ sở dữ liệu thành công";
+    } else {
+        $response = "Lỗi khi lưu dữ liệu vào cơ sở dữ liệu: " . $conn->error;
+    }
 }
 
 
