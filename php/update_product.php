097fc9079709d6b06fb483807e8e5673c62663a2
BachNguyen02122004 <bachtramtinh04@gmail.com>
2023-10-07 16:51:42 +0700

    fix id

--- php/update_product.php
+++ php/update_product.php
@@ -14,22 +14,41 @@ if ($conn->connect_error) {
 }
 
 
+$query = "SELECT MAX(id) AS max_id FROM cart";
+$result = mysqli_query($conn, $query);
+$row = mysqli_fetch_assoc($result);
+$maxId = $row['max_id'];
+
+
+$newId = $maxId + 1;
+
+
 $data = json_decode(file_get_contents("php://input"));
 
+$check = "SELECT * FROM cart WHERE productId = '$data->productId'";
+$result = mysqli_query($conn, $check);
 
-$sql = "INSERT INTO cart (id, productId, productName, productImage, quantity) VALUES ('$data->id', '$data->productId', '$data->name', '$data->image', '$data->quantity')";
+// Thêm vào giỏ hàng
+if ($result->num_rows == 0) {
+    $sql = "INSERT INTO cart (id, productId, productName, productImage, quantity) VALUES ('$newId', '$data->productId', '$data->name', '$data->image', '$data->quantity')";
 
-if ($conn->query($sql) === TRUE) {
-    $response = "Dữ liệu đã được lưu vào cơ sở dữ liệu thành công";
+    if ($conn->query($sql) === TRUE) {
+        $response = "Dữ liệu đã được lưu vào cơ sở dữ liệu thành công";
+    } else {
+        $response = "Lỗi khi lưu dữ liệu vào cơ sở dữ liệu: " . $conn->error;
+    }
+
 } else {
-    $response = "Lỗi khi lưu dữ liệu vào cơ sở dữ liệu: " . $conn->error;
+    $sql = "UPDATE cart SET quantity = (quantity + $data->quantity) WHERE productId = '$data->productId'";
+
+    if ($conn->query($sql) === TRUE) {
+        $response = "Dữ liệu đã được lưu vào cơ sở dữ liệu thành công";
+    } else {
+        $response = "Lỗi khi lưu dữ liệu vào cơ sở dữ liệu: " . $conn->error;
+    }
 }
-
-
-
 
 $conn->close();
 
-
 echo json_encode($response);
-?> 
+?>
