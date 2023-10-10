df7fdc951126eb507f9e426666cb48bac5da5e49
TrMiDUc <22026525@vnu.edu.vn>
2023-10-11 02:25:31 +0900

    update

--- js/cart.js
+++ js/cart.js
@@ -1,39 +1,74 @@
 
 const decreaseButtons = document.querySelectorAll('.item-down-up[aria-label="Decrease"]');
-  const increaseButtons = document.querySelectorAll('.item-down-up:not([aria-label="Decrease"])');
-  const inputElements = document.querySelectorAll('.text-number');
-  const real_coins = Array.from(document.querySelectorAll(".coin-item-cart")).map(element => element.textContent);
-  const coinsElement = document.querySelectorAll("#coin-number");
-  const coins = [];
-  let total_coin = parseInt(document.querySelector(".real-coin").textContent.replace(/\D/g, ""));
-  
-document.addEventListener('DOMContentLoaded', function() {
-  console.log(total_coin);
-  
-  console.log(coins);
-  
+const increaseButtons = document.querySelectorAll('.item-down-up:not([aria-label="Decrease"])');
+const inputElements = document.querySelectorAll('.text-number');
+const real_coins = Array.from(document.querySelectorAll(".coin-item-cart")).map(element => element.textContent);
+const coinsElement = document.querySelectorAll("#coin-number");
+const coins = [];
+let total_coin = parseInt(document.querySelector(".real-coin").textContent.replace(/\D/g, ""));
+
+document.addEventListener('DOMContentLoaded', function () {
+  // console.log(total_coin);
+  // console.log(coins);
+
   handleDelete();
-  
+
   decreaseButtons.forEach(function (button, index) {
     button.addEventListener('click', function () {
       let value = parseInt(inputElements[index].value);
-  
+
       if (value > 1) {
         value--;
         inputElements[index].value = value;
         coins[index] = parseInt(real_coins[index].replace(/\D/g, "")) * inputElements[index].value;
         total_coin -= parseInt(real_coins[index].replace(/\D/g, ""));
+
+        var url = "../php/dataChange.php?index=" + encodeURIComponent(index + 1) + "&value=" + encodeURIComponent(value);
+        fetch(url, {
+          method: 'POST', // Use POST method to send data
+          headers: {
+            'Content-Type': 'application/json' // Set content type to JSON
+          }
+        })
+          .then(response => response.json())
+          .then(data => {
+            // Handle the response from the PHP file
+            console.log(data);
+          })
+          .catch(error => {
+            // Handle errors
+            console.error('Error:', error);
+          });
+
         updateTotalPrice(index);
         updateTotalCoin();
       }
     });
   });
-  
+
   // Thiết lập sự kiện khi nhấn nút tăng
   increaseButtons.forEach(function (button, index) {
     button.addEventListener('click', function () {
       let value = parseInt(inputElements[index].value);
       value++;
+
+      var url = "../php/dataChange.php?index=" + encodeURIComponent(index + 1) + "&value=" + encodeURIComponent(value);
+      fetch(url, {
+        method: 'POST', // Use POST method to send data
+        headers: {
+          'Content-Type': 'application/json' // Set content type to JSON
+        }
+      })
+        .then(response => response.json())
+        .then(data => {
+          // Handle the response from the PHP file
+          console.log(data);
+        })
+        .catch(error => {
+          // Handle errors
+          console.error('Error:', error);
+        });
+
       inputElements[index].value = value;
       coins[index] = parseInt(real_coins[index].replace(/\D/g, "")) * inputElements[index].value;
       total_coin += parseInt(real_coins[index].replace(/\D/g, ""));
@@ -43,60 +78,59 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 });
 
-  function updateTotalPrice(index) {
-    
-    const quantity = inputElements[index].value;
-    const totalPriceElement = document.querySelectorAll('.coin-number')[index];
-    const totalPrice = coins[index];
-  
-    totalPriceElement.textContent = '₫' + totalPrice.toLocaleString('vi-VN');
-    
-  }
+function updateTotalPrice(index) {
 
-  function updateTotalCoin() {
-    const totalCoinElement = document.querySelector('.real-coin');
-    
-    const formattedTotalCoin = total_coin.toLocaleString('vi-VN');
-    console.log(formattedTotalCoin);
-    totalCoinElement.textContent =   "₫" + formattedTotalCoin;
-  }
+  const quantity = inputElements[index].value;
+  const totalPriceElement = document.querySelectorAll('.coin-number')[index];
+  const totalPrice = coins[index];
+
+  totalPriceElement.textContent = '₫' + totalPrice.toLocaleString('vi-VN');
+
+}
+
+function updateTotalCoin() {
+  const totalCoinElement = document.querySelector('.real-coin');
+
+  const formattedTotalCoin = total_coin.toLocaleString('vi-VN');
+  //console.log(formattedTotalCoin);
+  totalCoinElement.textContent = "₫" + formattedTotalCoin;
+}
 
 
-  function handleDelete() {
-    
-    const eraseProduct = document.querySelectorAll('.erase-product');
-  
-    eraseProduct.forEach(btn => {
-      btn.addEventListener('click', () => {
-        const productId = btn.closest('.product-info').querySelector('#productId').textContent;
-        console.log(productId);
-        const formProduct = {
-          productId: productId
-        };
-        deleteProduct(formProduct);
-        
-      });
+function handleDelete() {
+
+  const eraseProduct = document.querySelectorAll('.erase-product');
+
+  eraseProduct.forEach(btn => {
+    btn.addEventListener('click', () => {
+      const productId = btn.closest('.product-info').querySelector('#productId').textContent;
+      console.log(productId);
+      const formProduct = {
+        productId: productId
+      };
+      deleteProduct(formProduct);
+
     });
-  };
-  
-    // xóa product id dùng call api
-    function deleteProduct(id) {
-      fetch('../php/delete_product.php', {
-        method: 'DELETE',
-        body: JSON.stringify(id),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      })
-        .then(response => response.json())
-        .then(data => {
-          // Xử lý phản hồi từ server (nếu cần)
-          console.log(data);
-          window.location.href = '../php/cart.php';
-        })
-        
-      
-    }
+  });
+};
+
+// xóa product id dùng call api
+function deleteProduct(id) {
+  fetch('../php/delete_product.php', {
+    method: 'DELETE',
+    body: JSON.stringify(id),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then(response => response.json())
+    .then(data => {
+      // Xử lý phản hồi từ server (nếu cần)
+      console.log(data);
+      window.location.href = '../php/cart.php';
+    })
 
 
-    
+}
+
+
--- php/cart.php
+++ php/cart.php
@@ -37,8 +37,9 @@ $price = number_format($row['price'], 0, ',', '.');
     <link rel="stylesheet" href="../css/cart.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <script defer src="../js/cart.js"></script>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
 </head>
 
@@ -466,7 +467,5 @@ $mysqli->close();
                 </div>
             </div>
     </div>
-    
-    <script src="../js/cart.js"></script>
 </body>
 </html>
